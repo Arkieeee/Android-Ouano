a7975effc860d78e1af411744ede1a018aa2e5bb
Arkieeee <ouanoark@gmail.com>
2023-03-10 16:58:06 +0800

    Assignment Array continuation

--- app/src/main/java/com/example/helloandroid/FourthActivity.java
+++ app/src/main/java/com/example/helloandroid/FourthActivity.java
@@ -2,7 +2,11 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class FourthActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class FourthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+
+        Button buttonList = (Button) findViewById(R.id.button2);
+        buttonList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FourthActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
