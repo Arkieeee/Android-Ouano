88d449f8026e7cd238655ee999e587c12e908097
Arkieeee <ouanoark@gmail.com>
2023-03-11 11:40:12 +0800

    Frag Activity

--- app/src/main/java/com/example/helloandroid/MainFrag.java
+++ app/src/main/java/com/example/helloandroid/MainFrag.java
@@ -1,14 +1,28 @@
 package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import androidx.fragment.
 public class MainFrag extends AppCompatActivity {
-
+    Button firstFragment, secondFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_frag);
+
+        firstFragment = (Button) findViewById(R.id.button3);
+        secondFragment = (Button) findViewById(R.id.button4);
+
+
+
+        firstFragment.setOnClickListener((new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+        }
     }
 }
