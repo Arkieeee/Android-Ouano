c0649fcddbe1926e87810b4e92e7eb5358deb563
Arkieeee <ouanoark@gmail.com>
2023-03-18 12:29:55 +0800

    Action bar March 18 2023

---
+++ app/src/main/java/com/example/helloandroid/ActionBar.java
@@ -0,0 +1,72 @@
+package com.example.helloandroid;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ActionBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_bar);
+
+        androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setTitle("Arki");
+        actionBar.setSubtitle("Mao ni Action Bar");
+        actionBar.setIcon(R.mipmap.ic_launcher);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                showAlertDialog();
+                break;
+            case R.id.refresh:
+                DialogFragment dialogFragment = new DialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "Fragment");
+                break;
+            case R.id.copy:
+                Toast.makeText(this, "Copy Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private void showAlertDialog() {
+        // Implement your alert dialog here
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert!")
+                .setMessage("Danger you are falling");
+
+
+        builder.setPositiveButton("I know!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/helloandroid/DialogFragment.java
@@ -0,0 +1,17 @@
+package com.example.helloandroid;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
+        return inflater.inflate(R.layout.fragment_dialog, container, false);
+    }
+}
